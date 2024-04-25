9d223505dfa0d0a2e2d20e258e394937eaf2a1f4
nmarmi <nicolocmarmi@gmail.com>
2024-04-25 17:07:44 +0200

    knowledge file is read and passed at beginning of every question
    probably sub-optimal, but good starting point for having an assistant that can access private knowledge and give precise answers

--- venv/main.py
+++ venv/main.py
@@ -7,44 +7,47 @@ load_dotenv()
 
 client = OpenAI()
 
-# Basic setup to test api is working:
-# completion = client.chat.completions.create(
-#   model="gpt-3.5-turbo",
-#   messages=[
-#     {"role": "system", "content": "You are a Human Resources representative, in charge of dealing with the data of all employees, specifically the holidays taken and how many are left."},
-#     {"role": "user", "content": "."}
-#   ]
-# )
-# print(completion.choices[0].message.content)
+knowledge_file = open("/Users/nicolomarmi/PycharmProjects/HRchatBot/venv/emplyee_info.txt", "r")
+knowledge = knowledge_file.read()
+question = f"Use this Json knowledge about employees to answer the question: \n{knowledge}\n\nQuestion: "
+question += input("How  can I help you?\n")
 
 # Initialize HR assistant
 assistant = client.beta.assistants.create(
     name="HR ChatBot",
     instructions="You are my company's HR representative, your main tasks are informing employees on upcoming holidays, personal holidays each employee has left in the year, and how many days sick they took this year",
     model="gpt-4-turbo",
+    tools=[{"type": "file_search"}],
 )
 
 # A Thread represents a conversation between a user and one or many Assistants.
 thread = client.beta.threads.create()
 
-# messages from user
+# get message from user
 message = client.beta.threads.messages.create(
     thread_id=thread.id,
     role="user",
-    content=input("Hi! How can I help you? \n")
+    content=question
 )
 
+# run model to generate response
 run = client.beta.threads.runs.create_and_poll(
     thread_id=thread.id,
     assistant_id=assistant.id,
-    instructions="The user is a junior employee"
+    instructions="The user is a junior employee",
 )
 
 if run.status == 'completed':
     messages = client.beta.threads.messages.list(
         thread_id=thread.id
     )
-    print(messages)
+    print(messages.data[0].content[0].text.value)
 else:
     print("oh oh, something went wrong")
     print(run.status)
+
+"""
+TODO:
+- pass knowledge (json file)
+- create thread botta e risposta
+"""
